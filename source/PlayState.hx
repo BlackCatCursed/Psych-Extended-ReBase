0e66fe2dc0d627917c9017623adc9e60feef5582
Rookie <135964507+BlackCatCursed@users.noreply.github.com>
2025-09-09 15:37:50 +0700

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3442,6 +3442,7 @@ class PlayState extends MusicBeatState
 			        if (health > 0.1) { // Or your desired minimum health value
             setProperty('health', getProperty('health') - 0.025);
         }
+        }
 
 			var char:Character = dad;
 			var animToPlay:String = singAnimations[Std.int(Math.abs(note.noteData))] + altAnim;
